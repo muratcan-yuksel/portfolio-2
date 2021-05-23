8134aeb4d846ab8ca3264c978b5e3560e23886a6
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-05-23 17:09:40 +0300

    playing with navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import Navbar from "./components/Navbar";
 export class App extends Component {
   render() {
-    return <div>yo</div>;
+    return (
+      <div>
+        <Navbar />
+      </div>
+    );
   }
 }
 
